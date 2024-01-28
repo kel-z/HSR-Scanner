5df88e56afa604f1af0e6daacca0cf11d8323415
Kelvin Zhao <icyaneon@gmail.com>
2024-01-27 16:24:39 -0800

    docs: misc

--- src/services/scanner/scanner.py
+++ src/services/scanner/scanner.py
@@ -123,6 +123,7 @@ class HSRScanner(QtCore.QObject):
 
         self.complete_signal.emit()
         self.log_signal.emit("Starting OCR process. Please wait...")
+        self.log_signal.disconnect()
 
         return {
             "source": "HSR-Scanner",
